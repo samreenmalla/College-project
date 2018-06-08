b53ba69a2823ce8c2fb2b4b99b889640c36a4f20
Samreen <Samreen.malla@gmail.com>
2018-06-08 22:36:45 +0545

    photos:edit,update,delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
 	before_action :authenticate_user!, only: [:new, :create]
+	before_action :is_owner?, only: [:edit, :update, :destroy]
 
 	def index
 		@posts = Post.all.order('created_at DESC')
@@ -19,9 +20,36 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		if @post.valid?
+			redirect_to root_path
+		else
+			render :edit, status: :unprocessable_entity
+		end
+	end
+
+	def destroy
+		@post =  Post.find(params[:id])
+		@post.destroy
+		redirect_to root_path
+	end
+
 	private
 
 	def post_params
 		params.require(:post).permit(:user_id, :photo, :caption)
 	end
+
+	def is_owner?
+		if Post.find(params[:id]).user != current_user
+			redirect_to root_path
+		end
+	end
+
 end
