a3ed743b1ebec4855f1f42b03ff011b8cc3a8dac
Samreen <Samreen.malla@gmail.com>
2018-06-08 13:17:17 +0545

    posts displayed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,27 @@
 class PostsController < ApplicationController
 
-  def index
+	before_action :authenticate_user!, only: [:new, :create]
+
+	def index
+		@posts = Post.all.order('created_at DESC')
 	end
 	
 	def new
 		@post = Post.new
 	end
 
+	def create
+		@post = current_user.posts.create(post_params)
+		if @post.valid?
+			redirect_to root_path
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
 
+	private
 
+	def post_params
+		params.require(:post).permit(:user_id, :photo, :caption)
+	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
+
+  belongs_to :user
+  validates :photo, :caption, :user_id, presence: true
   mount_uploader :photo, PhotoUploader
 end
