37333e6c0f70020fd1e29a2f41a2ebee7de51034
Samreen <Samreen.malla@gmail.com>
2018-07-14 11:43:01 +0545

    validations for comments set

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,20 @@
 class CommentsController < ApplicationController
 
-    def index
-    end
+	def create
+		@post = Post.find(params[:post_id])
+		@idea = Idea.find(params[:idea_id])
+		@comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
+		@comment = @idea.comments.create(comment_params.merge(user_id: current_user.id))
+		if @comment.valid?
+			redirect_to root_path
+		else
+			flash[:alert] = "Inavlid attributes."
+			redirect_to root_path
+		end
+  end
 
-    def create
-    end
-
-    def new
-    end
+	private 
+	def comment_params
+		params.require(:comment).permit(:text, :post_id)
+		params.require(:comment).permit(:text, :idea_id)
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
     belongs_to :post
     belongs_to :user
     belongs_to :idea
+
+    validates :text, :user_id, :post_id, presence: true
+    validates :text, :user_id, :idea_id, presence: true
 end
